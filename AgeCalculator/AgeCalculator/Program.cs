6d00ae3cde547b2181d372af571e8065f5560f95
mak025 <mak025@edu.zealand.dk>
2025-02-09 16:41:33 +0100

    Created Project; AgeCalculator
    Created a new project named AgeCalculator

---
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AgeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Task 7 Start");
+            Thread.Sleep(1000);
+            Console.ForegroundColor= ConsoleColor.White;
+            DateTime birthday = DateTime.ParseExact(readin, "ddMMyyyy", null);
+            Console.WriteLine($"{birthday}");
+        }
+    }
+}
