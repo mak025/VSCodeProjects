913f58b99e85aa03026a87f778b8c75dff301ce3
mak025 <mak025@edu.zealand.dk>
2025-02-10 14:44:40 +0100

    Completed Christmas Project
    Created project app for calculating days till christmas

---
+++ Christmas/Christmas/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Christmas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Task 8 Start");
+            Console.ForegroundColor = ConsoleColor.White;
+            Thread.Sleep(1000);
+
+            DateTime christmas = new DateTime(2025, 12, 24);
+
+            Console.WriteLine("Enter a date in the format dd-MM-yyyy");
+            DateTime input = DateTime.Parse(Console.ReadLine());
+            double daysUntilChristmas = christmas.Subtract(input).TotalDays;
+            Thread.Sleep(1000);
+            Console.Write("Next christmas is: 24-12-2025. There are ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write($"{daysUntilChristmas}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" days left till Christmas");
+        }
+    }
+}
