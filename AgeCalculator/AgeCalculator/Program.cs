f16828850c73513a2ece8c11af75897cb3d2b8da
mak025 <mak025@edu.zealand.dk>
2025-02-10 12:24:02 +0100

    Project Created
    Project AgeCalculator has been created.
    
    Fix needed: Currently displays the total amount of days the person has lived. Needs to be displayed in a format of `X years old and y days`

---
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AgeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Task 7 Start");
+            Thread.Sleep(1000);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Please enter the birthday of the person you are assisting in the format: dd-MM-yyyy");
+            DateTime birthday = DateTime.Parse(Console.ReadLine());
+            TimeSpan difference = DateTime.Now - birthday;
+            int years = (int)Math.Floor(difference.TotalDays / 365.2425);
+            int days = (int)Math.Floor(difference.TotalDays);
+            Console.WriteLine($"The customer is {years} + years old and {days}");
+
+        }
+    }
+}
