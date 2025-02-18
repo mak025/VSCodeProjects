afae819709019cd50c082b4fc7737f5aa14541a6
mak025 <mak025@edu.zealand.dk>
2025-02-18 12:36:41 +0100

    Created new SubRepo
    Created a subsidiary repo for arrays and another for class test coding

---
+++ Taskset - Arrays/Class_Test/AvgTemp_Array/AvgTemp_Array/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AvgTemp_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dayTemp = { 7, 4, 5, 5, 5, 4, 4, 4 };
+            double sum = 0;
+            double maxTemp = 0;
+            double minTemp = 0;
+            for (int i = 0; i < dayTemp.Length; i++)
+            {
+                sum += dayTemp[i];
+                if (dayTemp[i] > maxTemp )
+                {
+                    maxTemp = dayTemp[i]; 
+                }
+                if (dayTemp[i] < minTemp)
+                {
+                    minTemp = dayTemp[i];
+                }
+            }
+            double avgtemp = sum / dayTemp.Length;
+            Console.WriteLine($"The avarage temperature this week is {avgtemp}");
+            Console.WriteLine($"The max temperature this week is {maxTemp} and the min temperature is {minTemp}");
+        }
+    }
+}
