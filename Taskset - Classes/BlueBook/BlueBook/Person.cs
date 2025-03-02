54e920bdc1520d61cd8d5c87f1c69d2e43b84a47
mak025 <mak025@edu.zealand.dk>
2025-03-02 22:05:35 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ Taskset - Classes/BlueBook/BlueBook/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBook
+{
+    public class Person
+    {
+        string _Name; //what is this persons name?
+        string _Gender; //is this person male or female?
+        int _Age; //how old is this person?
+        string _Profession; //what is the persons profession?
+        string _StudyField; //what is the person studying or educated in?
+
+        public Person(string name, string gender, int age, string profession, string study)
+        {
+            _Name = name;
+            _Gender = gender;
+            _Age = age;
+            _Profession = profession;
+            _StudyField = study;
+        }
+
+        public override string ToString()
+        {
+            return $"This person's name is {_Name}, they are {_Age} years old, identify as {_Gender}, work as {_Profession}, and studied {_StudyField}.";
+        }
+
+    }
+
+
+
+}
---
+++ Taskset - Classes/BlueBook/BlueBook/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BlueBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person carl = new Person("Carl", "Male", 32, "Steelworks", "craftsmanship");
+            
+            Console.WriteLine(carl);
+        }
+    }
+}
